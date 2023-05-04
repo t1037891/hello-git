8dcc3e0f0d5351a19bbcf4d3fb1da7e5a5d384e8
Camila Souza <131930817+t1037891@users.noreply.github.com>
2023-05-03 21:35:28 -0300

    Alteração na saudação

--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
-nome = input("Digite seu nome")
+nome = input("Digite seu nome completo")
 print("Boa noite")
-print("Seja bem vindo " + nome)
+print("Seja Muito Bem Vindo " + nome)
 print("Obrigado por visitar nosso site")
